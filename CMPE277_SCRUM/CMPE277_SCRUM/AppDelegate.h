a77d463fcf0a86c42ee88cd7509d17802f85c6aa
Sruthi <sruthi.mgr@gmail.com>
2013-04-15 16:00:10 -0700

    commit

---
+++ CMPE277_SCRUM/CMPE277_SCRUM/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CMPE277_SCRUM
+//
+//  Created by apoovekurussi on 4/15/13.
+//  Copyright (c) 2013 SJSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
